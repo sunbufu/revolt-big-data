b2b0caf0d5c51df72686e35379a4d5af29eff176
a123456 <sunhaisheng@guazi.com>
2018-04-23 19:28:47 +0800

    update start.sh

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "server starting"
-nohup java -jar revolt-big-data-0.0.1-SNAPSHOT.jar &
+BIN_DIR=`pwd`
+nohup java -jar $BIN_DIR/revolt-big-data-0.0.1-SNAPSHOT.jar &
 echo $! > rbd.pid
 echo "success"
